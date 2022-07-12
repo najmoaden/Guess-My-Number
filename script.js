31dd59a65e4fa7cfdcd178026cdf47feaa733df2
najmoaden <78856004+najmoaden@users.noreply.github.com>
2022-07-12 17:23:30 +0300

    Coding Challenge#1 -- After

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 //For the check button
@@ -54,10 +54,13 @@ document.querySelector('.check').addEventListener('click', function () {
 
 //For the again button
 document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').value = 20;
-  document.querySelector('.guess').value = 0;
-  document.querySelector('.number').textContent = secretNumber;
 });
